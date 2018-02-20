1a1bec214dd429bd502f396daeb35829d5c4bd24
Andrey Antukh <niwi@niwi.nz>
2018-02-20 23:56:53 +0100

    Improve timing indicator

--- Screeners/Timing.cs
+++ Screeners/Timing.cs
@@ -4,15 +4,8 @@ using cAlgo.API.Internals;
 using cAlgo.API.Indicators;
 
 namespace cAlgo.Indicators {
-    [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class Timing : Indicator {
-
-        [Output("Result", Color = Colors.Orange, PlotType = PlotType.Line)]
-        public IndicatorDataSeries Result { get; set; }
-
-        [Output("Result2", Color = Colors.Green, PlotType = PlotType.Line)]
-        public IndicatorDataSeries Result2 { get; set; }
-
         private MacdCrossOver lmacd;
         private MacdCrossOver rmacd;
 
@@ -21,11 +14,9 @@ namespace cAlgo.Indicators {
 
         private MarketSeries rseries;
         private TimeFrame reftf;
-        private int minimumIndex = 20;
 
-        //private string upArrow = "↑";
-        //private string downArrow = "↓";
-        //private double arrowOffset;
+        public IndicatorDataSeries Reference { get; set; }
+        public IndicatorDataSeries Local { get; set; }
 
         protected override void Initialize() {
             reftf = GetReferenceTimeframe(MarketSeries.TimeFrame);
@@ -39,37 +30,63 @@ namespace cAlgo.Indicators {
 
             Print("Load reference time frame: {0}", reftf);
 
+            Reference = CreateDataSeries();
+            Local = CreateDataSeries();
+
+            var result = CalculateMarketTiming();
+            Reference[MarketSeries.Close.Count - 1] = result.Item1;
+            Local[MarketSeries.Close.Count - 1] = result.Item2;
         }
 
         public override void Calculate(int index) {
-            if (index < minimumIndex) return;
-            if (!IsLastBar) {
-                Result[index] = CalculateLocalTiming(index);
-                Result2[index] = CalculateReferenceTiming(index);
+            if (IsRealTime) {
+                var result = CalculateMarketTiming();
+                Reference[index] = result.Item1;
+                Local[index] = result.Item2;
             }
-
-            //var rtiming = CalculateReferenceTiming(index);
-            //ChartObjects.DrawText("test", String.Format("Timing: {0} {1}", rtiming, Result[index]), StaticPosition.TopLeft, Colors.Black);
         }
 
-        public int CalculateLocalTiming(int index) {
-            //Print("calculating for index: {0} and is real time?: {1}", index, IsRealTime ? "yes" : "no");
-            if (lwma150.Result.IsRising()) {
-                if (lmacd.Histogram[index] > 0 && lmacd.Signal[index] > 0) {
+        private Tuple<int,int> CalculateMarketTiming() {
+            var local = CalculateLocalTiming();
+            var reference = CalculateReferenceTiming();
+
+            //Print("calculating market timing | Is real time?: {0} | Market Timing: {1} {2}", IsRealTime ? "yes" : "no", reference, local);
+
+            var msg = string.Format("Market timing: {0} {1}", reference, local);
+            ChartObjects.RemoveObject("test");
+            ChartObjects.DrawText("test", msg, StaticPosition.TopCenter, Colors.Black);
+
+           return new Tuple<int, int>(reference, local);
+        }
+
+        private bool IsTrendUp(MarketSeries series, WeightedMovingAverage wma) {
+            var close = series.Close.LastValue;
+            var value = wma.Result.LastValue;
+
+            if (value < close) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public int CalculateLocalTiming() {
+            if (IsTrendUp(MarketSeries, lwma150)) {
+                if (lmacd.Histogram.LastValue > 0 && lmacd.Signal.LastValue > 0) {
                     return 1;
-                } else if (lmacd.Histogram[index] > 0 && lmacd.Signal[index] < 0) {
+                } else if (lmacd.Histogram.LastValue > 0 && lmacd.Signal.LastValue < 0) {
                     return 4;
-                } else if (lmacd.Histogram[index] <= 0 && lmacd.Signal[index] >= 0) {
+                } else if (lmacd.Histogram.LastValue <= 0 && lmacd.Signal.LastValue >= 0) {
                     return 2;
                 } else {
                     return 3;
                 }
             } else {
-                if (lmacd.Histogram[index] < 0 && lmacd.Signal[index] < 0) {
+                if (lmacd.Histogram.LastValue < 0 && lmacd.Signal.LastValue < 0) {
                     return -1;
-                } else if (lmacd.Histogram[index] < 0 && lmacd.Signal[index] > 0) {
+                } else if (lmacd.Histogram.LastValue < 0 && lmacd.Signal.LastValue > 0) {
                     return -4;
-                } else if (lmacd.Histogram[index] >= 0 && lmacd.Signal[index] <= 0) {
+                } else if (lmacd.Histogram.LastValue >= 0 && lmacd.Signal.LastValue <= 0) {
                     return -2;
                 } else {
                     return -3;
@@ -77,46 +94,28 @@ namespace cAlgo.Indicators {
             }
         }
 
-        public int CalculateReferenceTiming(int lindex) {
-            var index = GetIndexByDate(rseries, MarketSeries.OpenTime[lindex]);
-
-            if (IsRising(rwma150.Result, index)) {
-                if (rmacd.Histogram[index] > 0 && rmacd.Signal[index] > 0) {
+        public int CalculateReferenceTiming() {
+            if (IsTrendUp(rseries, rwma150)) {
+                if (rmacd.Histogram.LastValue > 0 && rmacd.Signal.LastValue > 0) {
                     return 1;
-                } else if (rmacd.Histogram[index] > 0 && rmacd.Signal[index] < 0) {
+                } else if (rmacd.Histogram.LastValue > 0 && rmacd.Signal.LastValue < 0) {
                     return 4;
-                } else if (rmacd.Histogram[index] <= 0 && rmacd.Signal[index] >= 0) {
+                } else if (rmacd.Histogram.LastValue <= 0 && rmacd.Signal.LastValue >= 0) {
                     return 2;
                 } else {
                     return 3;
                 }
             } else {
-                if (rmacd.Histogram[index] < 0 && rmacd.Signal[index] < 0) {
+                if (rmacd.Histogram.LastValue < 0 && rmacd.Signal.LastValue < 0) {
                     return -1;
-                } else if (rmacd.Histogram[index] < 0 && rmacd.Signal[index] > 0) {
+                } else if (rmacd.Histogram.LastValue < 0 && rmacd.Signal.LastValue > 0) {
                     return -4;
-                } else if (rmacd.Histogram[index] >= 0 && rmacd.Signal[index] <= 0) {
+                } else if (rmacd.Histogram.LastValue >= 0 && rmacd.Signal.LastValue <= 0) {
                     return -2;
                 } else {
                     return -3;
                 }
             }
-        }
-
-        private int GetIndexByDate(MarketSeries series, DateTime time) {
-            for (int i = series.Close.Count - 1; i > 0; i--) {
-                DateTime closeTime;
-                if (reftf == TimeFrame.Hour) {
-                    closeTime = series.OpenTime[i].AddHours(1);
-                } else {
-                    closeTime = series.OpenTime[i].AddDays(1);
-                }
-
-                if (time >= series.OpenTime[i] && time < closeTime) {
-                    return i;
-                }
-            }
-            return -1;
         }
 
         public TimeFrame GetReferenceTimeframe(TimeFrame tf) {
@@ -124,25 +123,13 @@ namespace cAlgo.Indicators {
                 return TimeFrame.Daily;
             } else if (tf == TimeFrame.Minute5) {
                 return TimeFrame.Hour;
+            } else if (tf == TimeFrame.Daily) {
+                return TimeFrame.Weekly;
+            } else if (tf == TimeFrame.Weekly) {
+                return TimeFrame.Weekly;
             } else {
                 return TimeFrame.Hour;
             }
-        }
-
-        private bool IsRising(DataSeries data, int index) {
-            double sum = 0;
-            int periods = minimumIndex;
-            int counter = 0;
-
-            for (int i = index - periods; i <= index; i++) {
-                var val = data[i];
-                if (!double.IsNaN(val)) {
-                    sum += data[i];
-                    counter++;
-                }
-            }
-
-            return (sum / counter) > data[index-periods];
         }
     }
 }
